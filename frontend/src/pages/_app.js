b422e67be23b4fc110b2e28575521f61034d555c
guillermo <guillermoforerosuarez@gmail.com>
2024-08-07 12:39:12 -0500

    implements document to use head and fix babel problems

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-
 import { ProductsProvider } from "@/context/ProductsContext";
 
 import RootLayout from "../app/layout";
@@ -8,15 +6,6 @@ import "./styles/index.scss";
 
 const App = ({ Component, pageProps }) => (
   <>
-    <Head>
-      <title>Beer store</title>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
     <RootLayout>
       <ProductsProvider productsList={pageProps?.productsList}>
         <Component {...pageProps} />
